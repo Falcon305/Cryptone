163595f02964ee5f00f4dc89b3879d5061966b3c
Hatim Razani <hrazani@e1r10p1.1337.ma>
2020-11-17 04:28:40 +0100

    Updates

---
+++ precache-manifest.a58ae96ef8e0a72dce6e7c496ecf1919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d0949c06b7e5f4c8235b5fad5634af",
+    "url": "/Cryptone/index.html"
+  },
+  {
+    "revision": "da1b9557b49a7744d836",
+    "url": "/Cryptone/static/css/main.b0a26c83.chunk.css"
+  },
+  {
+    "revision": "43c6ca1b69e79262f854",
+    "url": "/Cryptone/static/js/2.bd2c7599.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cryptone/static/js/2.bd2c7599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1b9557b49a7744d836",
+    "url": "/Cryptone/static/js/main.1aa00fa9.chunk.js"
+  },
+  {
+    "revision": "0b2f6e2be09d5f9cbc03",
+    "url": "/Cryptone/static/js/runtime-main.27ea013b.js"
+  }
+]);
